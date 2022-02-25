ae6cc236a4d9ab53b2fdd5fe9617e651853f28ec
Ivana Pavlovic <ivanapavlovic314@users.noreply.github.com>
2022-02-25 22:17:02 +0100

    initial commit

---
+++ src/main/java/com/springframework/ippetclinic/IpPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.ippetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IpPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springframework/ippetclinic/IpPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.ippetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
