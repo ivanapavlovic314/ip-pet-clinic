9651b6db1307f26f1a10ca8f2de1bf3abeb3b9bc
Ivana Pavlovic <ivanapavlovic314@users.noreply.github.com>
2022-03-04 22:23:29 +0100

    Crud Service commit. closes #10

--- pet-clinic-data/src/main/java/com/springframework/ippetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/springframework/ippetclinic/services/VetService.java
@@ -2,11 +2,6 @@ package com.springframework.ippetclinic.services;
 
 import com.springframework.ippetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long>{
 
-public interface VetService {
-
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
 }
